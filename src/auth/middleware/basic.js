d5d550b17fe76af6644460a64ec8c9eee78cca59
oebitw <engomarewies@gmail.com>
2021-06-11 11:28:03 +0300

    Auth and API

---
+++ src/auth/middleware/basic.js
@@ -0,0 +1,34 @@
+'use strict';
+
+//////////////////////////
+////// Dependencies /////
+////////////////////////
+
+const base64 = require('base-64');
+
+///////////////////////
+////// Imports   /////
+/////////////////////
+
+const User = require('../models/users.js');
+
+
+module.exports = async (req, res, next) => {
+  if (!req.headers.authorization || !req.headers.authorization === 'Basic') {
+    next('authorization header is not provided');
+    return;
+  }
+  try {
+    const basic = req.headers.authorization.split(' ').pop();
+
+    const [username, password] = base64.decode(basic).split(':');
+
+    const authenticatedUser = await User.authenticateBasic(username, password);
+
+    req.user = authenticatedUser;
+
+    next();
+  } catch (error) {
+    res.status(403).send('Invalid Login');
+  }
+};
---
+++ src/auth/models/users.js
@@ -0,0 +1,134 @@
+'use strict';
+
+//////////////////////////
+////// Dependencies /////
+////////////////////////
+
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+
+////////////////////
+////// Schema /////
+//////////////////
+
+
+const SECRET = process.env.SECRET;
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      min: 3,
+      max: 20,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    profilePicture: {
+      type: String,
+      default: '',
+    },
+    coverPicture: {
+      type: String,
+      default: '',
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    followings: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    desc: {
+      type: String,
+      max: 50,
+    },
+    city: {
+      type: String,
+      max: 50,
+    },
+    from: {
+      type: String,
+      max: 50,
+    },
+    relationship: {
+      type: Number,
+      enum: [1, 2, 3],
+    },
+  },
+  { timestamps: true },
+);
+
+
+
+UserSchema.virtual('token').get(function () {
+  let tokenObject = {
+    username: this.username,
+  };
+  return jwt.sign(tokenObject, SECRET, { expiresIn: '1hr' });
+});
+
+
+
+UserSchema.pre('save', async function () {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+});
+
+
+UserSchema.statics.authenticateBasic = async function (username, password) {
+
+  try {
+    const user = await this.findOne({ username });
+    const valid = await bcrypt.compare(password, user.password);
+    if (valid) {
+      return user;
+    }else{
+      throw new Error('Invalid User');
+    }
+  } catch (error) {
+    throw new Error(error.message);        
+  }
+    
+};
+
+UserSchema.statics.authenticateBearer =async function (token) {
+  console.log(SECRET,'SECRET From User Model');
+  try {
+    const payload = jwt.verify(token, SECRET);
+    
+    const user = await this.findOne({
+      username: payload.username,
+    });
+    if (user) {
+      return user;
+    } else {
+      throw new Error('invalid username from token');
+    }
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+  
+
+
+
+module.exports = mongoose.model('User', UserSchema);
