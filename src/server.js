89c4db3c1038e5af3e262b56290e4e601e4b110d
ahmad-ahmad <aa0796780751@gmail.com>
2021-06-12 14:01:21 +0300

    last edit OauthFac

--- src/server.js
+++ src/server.js
@@ -42,12 +42,12 @@ app.use(helmet());
 
 /////////Ahmad///////
 
-const path =require('path');
+// const path =require('path');
 
-app.use(express.static(path.join(__dirname,'/public')));
+// app.use(express.static(path.join(__dirname,'../public')));
 
 
-
+app.use(express.static('./public'));
 
 
 
