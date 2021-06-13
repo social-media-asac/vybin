e046131f08b7f35f8d9ad033f82831b3d7212776
oebitw <engomarewies@gmail.com>
2021-06-13 19:31:07 +0300

    fb-auth done

--- public/js/fb.js
+++ public/js/fb.js
@@ -2,7 +2,7 @@
 
 const authorizeUrl = '//www.facebook.com/v10.0/dialog/oauth';
 const options = {
-  client_id:'2989335868017667',
+  client_id:'572920123691689',
   redirect_uri:'http://localhost:3000/api/v1/auth/facebook',
   state:'{st=state123abc,ds=123456789}',
 };
