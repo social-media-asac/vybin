bc6cc5434c057ee9d3a51c89cfddcf5363abb950
ahmad-ahmad <aa0796780751@gmail.com>
2021-06-16 00:37:15 +0300

    fixed OAuth

--- public/js/fb.js
+++ public/js/fb.js
@@ -2,8 +2,8 @@
 
 const authorizeUrl = '//www.facebook.com/v10.0/dialog/oauth';
 const options = {
-  client_id:'572920123691689',
-  redirect_uri:'http://localhost:3000/api/v1/auth/facebook',
+  client_id:'595955778041151',
+  redirect_uri:'https://vybin.herokuapp.com/api/v1/auth/facebook',
   state:'{st=state123abc,ds=123456789}',
 };
 
