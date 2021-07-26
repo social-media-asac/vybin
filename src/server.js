14b62484fe69abbee18759e4dd206c84e9209b48
oebitw <engomarewies@gmail.com>
2021-07-26 15:05:09 +0300

    images

--- src/server.js
+++ src/server.js
@@ -76,10 +76,10 @@ const storage = multer.diskStorage({
     cb(null, '../public/images');
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname);
+    cb(null,req.body.name);
   },
 });
-// req.body.name
+
 
 const upload = multer({ storage: storage });
 
